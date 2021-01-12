44479032bc6e008756c0ad0028b37c9d2e220cf9
SonerCerkez <sonerqerkeziii@gmail.com>
2021-01-12 16:03:42 +0100

    new commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Body.css'
 import '../App.css'
 import './Button'
+import girl3 from '../assets/girl3.jpg'
 
 
 
@@ -15,17 +16,15 @@ function Body() {
             <p>ModelKos was built on the fundamental principles of supplying quality talent to quality clients. The number
                 of beautiful women in Kosovo is extensive. With all these talents around we  have decided to give them the chance to be models.
                 We are one of the fastest growing agency's in Kosovo.</p> 
-                <button className='button2'>Learn More</button>
-            <div className="">
-                <div>  
-                    <img src={('/images/girl2.jpg.jpg')} alt='model' />                                       
+                <button className='button2'>Read More</button>
+                
+                <div className='fotoja'>                     
+                <img src={girl3.jpg} alt="model2"/>
                 </div>
             </div>
         </div>
-        </div>
         
-        
-        
+               
     )
 }
 
