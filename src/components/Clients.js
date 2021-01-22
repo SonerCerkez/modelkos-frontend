b75e1244abb9decba32f58876275f3612a8c5630
SonerCerkez <sonerqerkeziii@gmail.com>
2021-01-22 22:26:35 +0100

    commit

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -1,11 +1,7 @@
 import './Clients.css'
 import '../App.css'
 import './Button.css'
-import img19 from '../assets/introduction.png'
-import img20 from '../assets/after.png'
-import img21 from '../assets/three.png'
-import img22 from '../assets/Path3.png'
-import img23 from '../assets/int.png'
+
 
 
 
